5d5335dab6f0b791a8aeb8e82d6943b41e7d31bb
official.rifkidermawan@gmail.com <namasayarifki.123>
2021-06-12 05:02:22 +0700

    +

--- resources/views/intro/index.blade.php
+++ resources/views/intro/index.blade.php
@@ -7,7 +7,7 @@
 	<title>Felicity</title>
 	<link rel="preconnect" href="https://fonts.gstatic.com">
 	<link href="https://fonts.googleapis.com/css2?family=Poppins&display=swap" rel="stylesheet">
-	<link rel="stylesheet" href="/css/app.css">
+	<link rel="stylesheet" href="{{asset('/css/app.css')}}">
 	<style>
         body {
             font-family: 'Kollektif', sans-serif;
