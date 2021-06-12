cb6e36d85b0322caa016a83699aa3a7851642efc
official.rifkidermawan@gmail.com <namasayarifki.123>
2021-06-12 17:07:46 +0700

    +

--- resources/views/intro/index.blade.php
+++ resources/views/intro/index.blade.php
@@ -7,6 +7,7 @@
 	<title>Felicity</title>
 	<link rel="preconnect" href="https://fonts.gstatic.com">
 	<link href="https://fonts.googleapis.com/css2?family=Poppins&display=swap" rel="stylesheet">
+	<link rel="shortcut icon" type="image/png" href="{{asset('favicon.png')}}"/>
 	<link rel="stylesheet" href="{{asset('/css/app.css')}}">
 	<style>
         body {
