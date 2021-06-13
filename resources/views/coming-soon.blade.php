71402dd46f371219eb5876e96b9430e0951a64f2
official.rifkidermawan@gmail.com <namasayarifki.123>
2021-06-13 10:46:06 +0700

    +

--- resources/views/coming-soon.blade.php
+++ resources/views/coming-soon.blade.php
@@ -7,7 +7,7 @@
 	<title>Felicity</title>
 	<link rel="preconnect" href="https://fonts.gstatic.com">
 	<link href="https://fonts.googleapis.com/css2?family=Poppins&display=swap" rel="stylesheet">
-	<link rel="stylesheet" href="{{asset('/css/app.css')}}">
+	<link rel="stylesheet" href="{{asset('/css/app.css?new')}}">
 	
 
 
