71a458fbb32a5a466ab68f35f45cc56a07d7a707
official.rifkidermawan@gmail.com <namasayarifki.123>
2021-06-13 23:46:59 +0700

    +

--- resources/views/intro/index.blade.php
+++ resources/views/intro/index.blade.php
@@ -5,7 +5,7 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<meta http-equiv="X-UA-Compatible" content="ie=edge">
 	<title>Felicity</title>
-	<meta name=" theme-color" content="#d65244">
+	<meta name="theme-color" content="#d65244">
 	<link rel="preconnect" href="https://fonts.gstatic.com">
 	<link href="https://fonts.googleapis.com/css2?family=Poppins&display=swap" rel="stylesheet">
 	<link rel="shortcut icon" type="image/png" href="{{asset('favicon.png')}}"/>
