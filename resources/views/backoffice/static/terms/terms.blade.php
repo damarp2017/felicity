04fa22c1e2f9a9551bb70452f6e0d22a2978fed4
HelfanzaNanda <elvannnd@gmail.com>
2021-06-29 12:11:19 +0700

    merge afif

--- resources/views/backoffice/static/terms/terms.blade.php
+++ resources/views/backoffice/static/terms/terms.blade.php
@@ -3,7 +3,6 @@
 @push('styles')
 <!-- Toastr -->
 <link rel="stylesheet" href="{{ asset('assets/adminlte/plugins/toastr/toastr.min.css') }}">
-<link href="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css" rel="stylesheet">
 <link href="https://cdn.jsdelivr.net/npm/summernote@0.8.18/dist/summernote.min.css" rel="stylesheet">
 @endpush
 
